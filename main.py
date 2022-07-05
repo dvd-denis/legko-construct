4cf8f8c0f61f24fa56c66094e178d62387bd7347
Denis <pikuldenis9@gmail.com>
2022-07-05 12:53:55 +0600

    Fix questions

--- main.py
+++ main.py
@@ -8,19 +8,12 @@ import base64
 def checkExist(api_url):
     try:
         with open('status.txt', 'r') as stats_file:
-        a: int = int(stats_file.readline())
-        req = requests.get("http://" + api_url + "/article/delete/" + str(a))
-        if req.status_code == 200:
-            print("Updated!!")
+            a: int = int(stats_file.readline())
+            req = requests.get("http://" + api_url + "/article/delete/" + str(a))
+            if req.status_code == 200:
+                print("Updated!")
     except Exception as e:
-        raise
-        
-class MyEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, (bytes, bytearray)):
-            return obj.decode("UTF8") # <- or any other encoding of your choice
-        # Let the base class default method raise the TypeError
-        return json.JSONEncoder.default(self, obj)
+        pass
 
 if __name__ == "__main__":
     config = toml.load("settings/config.toml")
@@ -41,26 +34,26 @@ if __name__ == "__main__":
         step = toml.load("steps/" + dir + "/config.toml")
         with open("steps/" + dir + "/content.html", "r") as step_content_file:
             step["content"] = step_content_file.read()
-        print(article_id)
         step["article_id"] = article_id
         step["num"] = int(dir)
 
         response = requests.post("http://" + api_url + "/step", data=json.dumps(step))
         step_id = response.json()
+        if (not step["question"]):
+            imgs = []
 
-        imgs = []
+            for image in os.listdir('steps/' + dir + "/images"):
+                with open('steps/' + dir + "/images/" + image, "rb") as image_file:
+                    img = {}
+                    img["step_id"] = step_id
+                    img["image"] = base64.b64encode(image_file.read()).decode("UTF8")
+                    img["image_name"] = image
+                    imgs.append(img)
+            
+            response = requests.post("http://" + api_url + "/images", data=json.dumps(imgs))
 
-        for image in os.listdir('steps/' + dir + "/images"):
-            with open('steps/' + dir + "/images/" + image, "rb") as image_file:
-                img = {}
-                img["step_id"] = step_id
-                img["image"] = base64.b64encode(image_file.read()).decode("UTF8")
-                img["image_name"] = image
-                imgs.append(img)
-        
-        response = requests.post("http://" + api_url + "/images", data=json.dumps(imgs))
-        if (response.status_code == 200):
-            with open('status.txt', 'w') as stats_file:
-                stats_file.write(str(step_id))
+    if (response.status_code == 200):
+        with open('status.txt', 'w') as stats_file:
+            stats_file.write(str(article_id))
 
-        print("Complete!")
+    print("Complete!")

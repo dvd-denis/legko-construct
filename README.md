9da40574f72030f0a8a94890b4ec4714546c91f2
Denis <pikuldenis9@gmail.com>
2022-07-07 12:54:06 +0600

    Add auth

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # legko-construct
+
+#### Env file
+- > rename `.env.template` to `.env`
+- > enter your key to `KEY`
--- main.py
+++ main.py
@@ -5,18 +5,28 @@ from time import process_time_ns
 import requests
 import toml
 import base64
+from dotenv import dotenv_values, load_dotenv 
 
-def checkExist(api_url, article_folder):
+def checkExist(api_url, article_folder, headers):
     try:
         with open("src/" + article_folder + '/status.txt', 'r') as stats_file:
             a: int = int(stats_file.readline())
-            req = requests.get("http://" + api_url + "/article/delete/" + str(a))
+            req = requests.post("http://" + api_url + "/article/delete/" + str(a), headers=headers)
             if req.status_code == 200:
                 print(article_folder + ": Updated!")
     except Exception as e:
         pass
 
 if __name__ == "__main__":
+
+    load_dotenv()
+
+    env_config = dotenv_values(".env")
+
+    headers = {
+        "Key": env_config["KEY"]
+    }
+
     global_config = toml.load("config.toml")
     api_url = global_config["ip"] + ":" + global_config["port"]   
     (_, article_folders, _) = os.walk("src").__next__()
@@ -26,14 +36,14 @@ if __name__ == "__main__":
 
         article = toml.load("src/" + article_folder + "/settings/config.toml")
 
-        checkExist(api_url, article_folder)
+        checkExist(api_url, article_folder, headers)
         
         with open("src/" + article_folder + "/icon/" + article['icon_name'], "rb") as image_file:
             icon = base64.b64encode(image_file.read()).decode("UTF8")
 
         article["icon"] = icon
 
-        response = requests.post("http://" + api_url + "/article", data=json.dumps(article))
+        response = requests.post("http://" + api_url + "/article", data=json.dumps(article), headers=headers)
         article_id = response.json()
 
         for dir in os.listdir("src/" + article_folder + '/steps'):
@@ -43,7 +53,7 @@ if __name__ == "__main__":
             step["article_id"] = article_id
             step["num"] = int(dir)
 
-            response = requests.post("http://" + api_url + "/step", data=json.dumps(step))
+            response = requests.post("http://" + api_url + "/step", data=json.dumps(step), headers=headers)
             step_id = response.json()
             imgs = []
 
@@ -55,7 +65,7 @@ if __name__ == "__main__":
                     img["image_name"] = image
                     imgs.append(img)
             if len(imgs) != 0:
-                response = requests.post("http://" + api_url + "/images", data=json.dumps(imgs))
+                response = requests.post("http://" + api_url + "/images", data=json.dumps(imgs), headers=headers)
 
         if (response.status_code == 200):
             with open("src/" + article_folder + '/status.txt', 'w') as stats_file:

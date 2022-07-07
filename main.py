851bb8116bd7faf64c25bf495f5cdb3325b5b3c8
Denis <pikuldenis9@gmail.com>
2022-07-07 12:05:56 +0600

    add src folder

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from cgitb import text
 import json
 import os
+from time import process_time_ns
 import requests
 import toml
 import base64
 
 def checkExist(api_url, article_folder):
     try:
-        with open(article_folder + '/status.txt', 'r') as stats_file:
+        with open("src/" + article_folder + '/status.txt', 'r') as stats_file:
             a: int = int(stats_file.readline())
             req = requests.get("http://" + api_url + "/article/delete/" + str(a))
             if req.status_code == 200:
@@ -18,13 +19,16 @@ def checkExist(api_url, article_folder):
 if __name__ == "__main__":
     global_config = toml.load("config.toml")
     api_url = global_config["ip"] + ":" + global_config["port"]   
+    (_, article_folders, _) = os.walk("src").__next__()
+    for article_folder in article_folders:
+        print(article_folders)
+        print(article_folder)
 
-    for article_folder in [d for d in os.listdir('.') if os.path.isdir(d) and not d.startswith(".")]:
-        article = toml.load(article_folder + "/settings/config.toml")
+        article = toml.load("src/" + article_folder + "/settings/config.toml")
 
         checkExist(api_url, article_folder)
         
-        with open(article_folder + "/icon/" + article['icon_name'], "rb") as image_file:
+        with open("src/" + article_folder + "/icon/" + article['icon_name'], "rb") as image_file:
             icon = base64.b64encode(image_file.read()).decode("UTF8")
 
         article["icon"] = icon
@@ -32,9 +36,9 @@ if __name__ == "__main__":
         response = requests.post("http://" + api_url + "/article", data=json.dumps(article))
         article_id = response.json()
 
-        for dir in os.listdir(article_folder + '/steps'):
-            step = toml.load(article_folder + "/steps/" + dir + "/config.toml")
-            with open(article_folder + "/steps/" + dir + "/content.html", "r") as step_content_file:
+        for dir in os.listdir("src/" + article_folder + '/steps'):
+            step = toml.load("src/" + article_folder + "/steps/" + dir + "/config.toml")
+            with open("src/" + article_folder + "/steps/" + dir + "/content.html", "r") as step_content_file:
                 step["content"] = step_content_file.read()
             step["article_id"] = article_id
             step["num"] = int(dir)
@@ -43,8 +47,8 @@ if __name__ == "__main__":
             step_id = response.json()
             imgs = []
 
-            for image in os.listdir(article_folder + '/steps/' + dir + "/images"):
-                with open(article_folder + '/steps/' + dir + "/images/" + image, "rb") as image_file:
+            for image in os.listdir("src/" + article_folder + '/steps/' + dir + "/images"):
+                with open("src/" + article_folder + '/steps/' + dir + "/images/" + image, "rb") as image_file:
                     img = {}
                     img["step_id"] = step_id
                     img["image"] = base64.b64encode(image_file.read()).decode("UTF8")
@@ -54,7 +58,7 @@ if __name__ == "__main__":
                 response = requests.post("http://" + api_url + "/images", data=json.dumps(imgs))
 
         if (response.status_code == 200):
-            with open(article_folder + '/status.txt', 'w') as stats_file:
+            with open("src/" + article_folder + '/status.txt', 'w') as stats_file:
                 stats_file.write(str(article_id))
 
         print(article_folder + ": Complete!")

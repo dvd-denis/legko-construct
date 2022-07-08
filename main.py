50f0454afb39c0c9bad8198b5ad7d9e5133d91b4
Denis <pikuldenis9@gmail.com>
2022-07-08 22:53:52 +0600

    Add tags and fix num

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ def checkExist(api_url, group_folder, headers):
     try:
         with open("src/" + group_folder + '/status.txt', 'r') as stats_file:
             a: int = int(stats_file.readline())
-            req = requests.post("https://" + api_url + "/group/delete/" + str(a), headers=headers)
+            req = requests.post(api_url + "/group/delete/" + str(a), headers=headers)
             if req.status_code == 200:
                 print(group_folder + ": Updated!")
     except Exception as e:
-        pass
+        print(e)
 
 if __name__ == "__main__":
 
@@ -38,27 +38,25 @@ if __name__ == "__main__":
 
         group["icon"] = icon
 
-        response = requests.post("https://" + api_url + "/group", data=json.dumps(group), headers=headers)
+        response = requests.post(api_url + "/group", data=json.dumps(group), headers=headers)
         group_id = response.json()
 
         for article_folder in os.listdir("src/" + group_folder + '/articles'):
             article = toml.load("src/" + group_folder + "/articles/" + article_folder + "/article.toml")
             article["group_id"] = group_id
             
-            response = requests.post("https://" + api_url + "/article/", data=json.dumps(article), headers=headers)
+            response = requests.post(api_url + "/article/", data=json.dumps(article), headers=headers)
             article_id = response.json()
             
-            num = 0
             for step_folder in os.listdir("src/" + group_folder + '/articles/' + article_folder + '/steps'):
 
                 step = toml.load("src/" + group_folder + '/articles/' + article_folder + '/steps/' + step_folder + "/step.toml")
                 with open("src/" + group_folder + '/articles/' + article_folder + '/steps/' + step_folder + "/content.html", "r") as step_content_file:
                     step["content"] = step_content_file.read()
                 step["article_id"] = article_id
-                step["num"] = num
-                num+=1
+                step["num"] = int(step_folder)
 
-                response = requests.post("https://" + api_url + "/step", data=json.dumps(step), headers=headers)
+                response = requests.post(api_url + "/step", data=json.dumps(step), headers=headers)
                 step_id = response.json()
                 imgs = []
 
@@ -70,7 +68,7 @@ if __name__ == "__main__":
                         img["image_name"] = image
                         imgs.append(img)
                 if len(imgs) != 0:
-                    response = requests.post("https://" + api_url + "/images", data=json.dumps(imgs), headers=headers)
+                    response = requests.post(api_url + "/images", data=json.dumps(imgs), headers=headers)
 
         if (response.status_code == 200):
             with open("src/" + group_folder + '/status.txt', 'w') as stats_file:
